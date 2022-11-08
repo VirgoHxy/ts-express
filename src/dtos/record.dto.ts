d90e6402499eabecc59e742c7923584a8a6f03fd
Wits.BenjaminHao <Wits.BenjaminHao@wistron.com>
2022-11-08 14:48:13 +0800

    feat: 调整

--- src/dtos/record.dto.ts
+++ src/dtos/record.dto.ts
@@ -1,6 +1,5 @@
 import { IsString, ValidateIf } from 'class-validator';
 
-// 一般可以直接在entity上定义，不需要单独创建dto
 export class GetRecordsTestDto {
   @IsString()
   @ValidateIf((_object, value) => value != undefined || value != '') // 如果false 则忽略其他验证
